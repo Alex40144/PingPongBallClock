62f5760466463fbbd9e29444e6d574512c6d11c0
Alex40144 <giddyraccoon@gmail.com>
2020-06-23 16:08:44 +0100

    added another missing comma

--- pingpongclock.py
+++ pingpongclock.py
@@ -61,7 +61,7 @@ def six(origin_x, origin_y):
 	return(D[origin_x][origin_y],D[origin_x][origin_y+1],D[origin_x][origin_y+2],D[origin_x][origin_y+3],D[origin_x+1][origin_y],D[origin_x+2][origin_y],D[origin_x+3][origin_y],D[origin_x+3][origin_y+1],D[origin_x+3][origin_y+2],D[origin_x+3][origin_y+3],D[origin_x+4][origin_y],D[origin_x+4][origin_y+3],D[origin_x+5][origin_y],D[origin_x+5][origin_y+3],D[origin_x+6][origin_y],D[origin_x+6][origin_y+1],D[origin_x+64][origin_y+2],D[origin_x+6][origin_y+3])
 
 def seven(origin_x, origin_y):
-	return(D[origin_x][origin_y],D[origin_x][origin_y+1],D[origin_x][origin_y+2],D[origin_x][origin_y+3],D[origin_x+1][origin_y+3],D[origin_x+2][origin_y+3],D[origin_x+3][origin_y+3],D[origin_x+4][origin_y+3]D[origin_x+5][origin_y+3],D[origin_x+6][origin_y+3])
+	return(D[origin_x][origin_y],D[origin_x][origin_y+1],D[origin_x][origin_y+2],D[origin_x][origin_y+3],D[origin_x+1][origin_y+3],D[origin_x+2][origin_y+3],D[origin_x+3][origin_y+3],D[origin_x+4][origin_y+3],D[origin_x+5][origin_y+3],D[origin_x+6][origin_y+3])
 
 def eight(origin_x, origin_y):
 	return(D[origin_x][origin_y],D[origin_x][origin_y+1],D[origin_x][origin_y+2],D[origin_x][origin_y+3],D[origin_x+1][origin_y],D[origin_x+1][origin_y+3],D[origin_x+2][origin_y],D[origin_x+2][origin_y+3],D[origin_x+3][origin_y],D[origin_x+3][origin_y+1],D[origin_x+3][origin_y+2],D[origin_x+3][origin_y+3],D[origin_x+4][origin_y],D[origin_x+4][origin_y+3],D[origin_x+5][origin_y],D[origin_x+5][origin_y+3],D[origin_x+6][origin_y],D[origin_x+6][origin_y+1],D[origin_x+64][origin_y+2],D[origin_x+6][origin_y+3])
