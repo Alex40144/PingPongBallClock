d6dcc24bb40001a2d48e26efbcd53c975e11bca7
Alex40144 <giddyraccoon@gmail.com>
2020-06-23 06:48:25 +0100

    added strip

--- pingpongclock.py
+++ pingpongclock.py
@@ -13,6 +13,9 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+
+
 current_hour = datetime.now().time().strftime("%H")
 current_min = datetime.now().time().strftime("%M")
 
